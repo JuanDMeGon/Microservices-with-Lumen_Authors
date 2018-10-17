64923c970b89f5c84cbc9f97247862be2d63f18c
JuanDMeGon <juandmegon@gmail.com>
2018-10-17 18:29:10 -0500

    Normalizing the authors’ microservice responses

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
+
     /**
      * Create a new controller instance.
      *
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser
+{
+    /**
+     * Build success response
+     * @param  string|array $data
+     * @param  int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json(['data' => $data], $code);
+    }
+
+    /**
+     * Build error responses
+     * @param  string|array $message
+     * @param  int $code
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+}
