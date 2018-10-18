f6ee27bb45b70c7ccb6c05e6a797c02d3b010622
JuanDMeGon <juandmegon@gmail.com>
2018-10-17 19:06:39 -0500

    Allowing editing an existing author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -68,6 +68,25 @@ class AuthorController extends Controller
      */
     public function update(Request $request, $author)
     {
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::findOrFail($author);
+
+        $author->fill($request->all());
+
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return $this->successResponse($author);
     }
 
     /**
