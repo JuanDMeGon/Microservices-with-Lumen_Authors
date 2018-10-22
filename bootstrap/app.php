7bb0d8e70528b33a0987adf8e2e49a317d4762d1
JuanDMeGon <juandmegon@gmail.com>
2018-10-22 15:45:26 -0500

    Authenticating direct access to the Lumen authors microservice

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,9 +59,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\AuthenticateAccess::class,
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
